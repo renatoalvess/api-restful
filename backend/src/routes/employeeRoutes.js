9f432826d5f9127dabaaaf5a5694daacdaefafae
JoseValberio <valberio.gold021@gmail.com>
2025-08-18 23:21:06 -0300

    Adicionada a rota GET

--- backend/src/routes/employeeRoutes.js
+++ backend/src/routes/employeeRoutes.js
@@ -5,4 +5,7 @@ const employeeController = require('../controllers/employeeController');
 // Rota para cadastrar um funcionário
 router.post('/', employeeController.createEmployee);
 
+// Rota para buscar todos os funcionários
+router.get('/buscar', employeeController.findEmployee);
+
 module.exports = router;
